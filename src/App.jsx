d045b3b3382f5061119af55349e6ec5d140befe9
olyverDev <aleh.verameichyk@instinctools.ru>
2019-04-18 17:54:07 +0300

    Add some content

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        {'Hello, I am bundled with rollup.js and deployed to GitLab Pages.'}
         { this.props.children }
       </div>
     );
