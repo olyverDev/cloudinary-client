759e6f4c386282dc8706cfc28edfe55b74a16452
olyverDev <aleh.verameichyk@instinctools.ru>
2019-05-28 23:18:45 +0300

    Add comment to dev requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8888/',
+  baseURL: 'http://localhost:8888/', // FIXME, it's default local server port
   withCredentials: false,
 });
